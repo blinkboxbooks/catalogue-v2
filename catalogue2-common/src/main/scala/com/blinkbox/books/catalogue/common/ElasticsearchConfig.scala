b18f6a3af753fbdc976b41b606a8b07300b923ef
Aldo Stracquadanio <aldos@blinkbox.com>
2014-12-15 17:44:32 +0000

    Introduce timeout for ES client and log unhandled failures

--- catalogue2-common/src/main/scala/com/blinkbox/books/catalogue/common/ElasticsearchConfig.scala
+++ catalogue2-common/src/main/scala/com/blinkbox/books/catalogue/common/ElasticsearchConfig.scala
@@ -1,14 +1,17 @@
 package com.blinkbox.books.catalogue.common
 
+import com.blinkbox.books.config._
 import com.typesafe.config.Config
+import scala.concurrent.duration.FiniteDuration
 
-case class ElasticsearchConfig(host: String, port: Int, clusterName: String, indexName: String)
+case class ElasticsearchConfig(host: String, port: Int, clusterName: String, indexName: String, timeout: FiniteDuration)
 
 object ElasticsearchConfig {
   def apply(config: Config, prefix: String = "elasticsearch"): ElasticsearchConfig = ElasticsearchConfig(
     host = config.getString(s"$prefix.host"),
     port = config.getInt(s"$prefix.port"),
     clusterName = config.getString(s"$prefix.clusterName"),
-    indexName = config.getString(s"$prefix.indexName")
+    indexName = config.getString(s"$prefix.indexName"),
+    timeout = config.getFiniteDuration(s"$prefix.timeout")
   )
 }
