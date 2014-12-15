df12993e3c8fc6dbc6d0d4a49dfa12f141416c50
csargent <chriss@blinkbox.com>
2014-12-15 13:31:29 +0000

    Links in search results always contain count and offset

--- catalogue2-search-public/src/test/scala/com/blinkbox/books/catalogue/searchv1/PageLinkerSpecs.scala
+++ catalogue2-search-public/src/test/scala/com/blinkbox/books/catalogue/searchv1/PageLinkerSpecs.scala
@@ -10,27 +10,33 @@ class PageLinkerSpecs extends FlatSpec with Matchers {
   val uri = Uri("http://somehost.com/resource?param=foo&otherParam=bar&count=5")
   val query = uri.query.toMap
 
-  def link(rel: String, uri: Uri) = Link(rel, uri.toString, None, None)
+  def link(rel: String, uri: Uri, offset: String, count: String) =
+    Link(rel, uri.withQuery(query.updated("count", count).updated("offset", offset)).toString, None, None)
 
   "The page linker" should "correctly build the 'this' link" in {
-    PageLinker.links(uri, Page(0, 10), 10) should equal(Seq(link("this", uri)))
+    PageLinker.links(uri, Page(0, 10), 10) should contain theSameElementsAs(
+      link("this", uri, "0", "10") :: Nil)
   }
 
   it should "correctly build the 'next' link if possible" in {
     PageLinker.links(uri, Page(0, 5), 10) should contain theSameElementsAs(
-      link("this", uri) :: link("next", uri.withQuery(query.updated("offset", "5").updated("count", "5"))) :: Nil)
+      link("this", uri, "0", "5") ::
+      link("next", uri, "5", "5") ::
+      Nil)
   }
 
   it should "correctly build the 'prev' link if possible" in {
     PageLinker.links(uri, Page(5, 5), 10) should contain theSameElementsAs(
-      link("this", uri) :: link("prev", uri.withQuery(query.updated("offset", "0").updated("count", "5"))) :: Nil)
+      link("this", uri, "5", "5") ::
+      link("prev", uri, "0", "5") ::
+      Nil)
   }
 
   it should "correctly build the all links if possible" in {
     PageLinker.links(uri, Page(10, 5), 20) should contain theSameElementsAs(
-      link("this", uri) ::
-      link("prev", uri.withQuery(query.updated("offset", "5").updated("count", "5"))) ::
-      link("next", uri.withQuery(query.updated("offset", "15").updated("count", "5"))) ::
+      link("this", uri, "10", "5") ::
+      link("prev", uri, "5", "5") ::
+      link("next", uri, "15", "5") ::
       Nil)
   }
 }
