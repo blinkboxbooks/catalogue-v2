e5b916c132aa101cbf2235a6d5460167e04b84a0
Aldo Stracquadanio <aldos@blinkbox.com>
2014-12-18 16:54:32 +0000

    CAT-137 Remove suggestions from the response when no suggestions are provided

--- catalogue2-search-public/src/test/scala/com/blinkbox/books/catalogue/searchv1/SpellcheckSpecs.scala
+++ catalogue2-search-public/src/test/scala/com/blinkbox/books/catalogue/searchv1/SpellcheckSpecs.scala
@@ -14,48 +14,50 @@ class SpellcheckSpecs extends FlatSpec with Matchers with ApiSpecBase {
     catalogueIndex indexAndCheck (f.universe, f.everything, f.simpleBook) andAwaitFor 10.seconds
   }
 
+  def suggestions(resp: BookSearchResponse): Seq[String] = resp.suggestions.getOrElse(Nil)
+
   "Providing a query to the search service" should "result in a query suggestion if it only contains a mis-spelled word" in {
     Get("/catalogue/search/books?q=uvinerse") ~> routes ~> check {
       status should equal(StatusCodes.OK)
-      responseAs[BookSearchResponse].suggestions should contain theSameElementsAs ("universe" :: Nil)
+      suggestions(responseAs[BookSearchResponse]) should contain theSameElementsAs ("universe" :: Nil)
     }
 
     Get("/catalogue/search/books?q=everithing") ~> routes ~> check {
       status should equal(StatusCodes.OK)
-      responseAs[BookSearchResponse].suggestions should contain theSameElementsAs ("everything" :: Nil)
+      suggestions(responseAs[BookSearchResponse]) should contain theSameElementsAs ("everything" :: Nil)
     }
   }
 
   it should "result in a query suggestion if it contains a mis-spelled word and a non-misspelled word" in {
     Get("/catalogue/search/books?q=uvinerse%20everything") ~> routes ~> check {
       status should equal(StatusCodes.OK)
-      responseAs[BookSearchResponse].suggestions should contain theSameElementsAs ("universe everything" :: Nil)
+      suggestions(responseAs[BookSearchResponse]) should contain theSameElementsAs ("universe everything" :: Nil)
     }
   }
 
   it should "result in a query suggestion if it contains multiple mis-spelled words" in {
     Get("/catalogue/search/books?q=uvinerse%20eveyrthing%20simple") ~> routes ~> check {
       status should equal(StatusCodes.OK)
-      responseAs[BookSearchResponse].suggestions should contain theSameElementsAs ("universe everything simple" :: Nil)
+      suggestions(responseAs[BookSearchResponse]) should contain theSameElementsAs ("universe everything simple" :: Nil)
     }
   }
 
   it should "not result in a query suggestion if the word is correctly spelled" in {
     Get("/catalogue/search/books?q=universe") ~> routes ~> check {
       status should equal(StatusCodes.OK)
-      responseAs[BookSearchResponse].suggestions shouldBe empty
+      responseAs[BookSearchResponse].suggestions should equal(None)
     }
 
     Get("/catalogue/search/books?q=everything") ~> routes ~> check {
       status should equal(StatusCodes.OK)
-      responseAs[BookSearchResponse].suggestions shouldBe empty
+      responseAs[BookSearchResponse].suggestions should equal(None)
     }
   }
 
   it should "not result in a query suggestion if the word is totally different from any title in the index" in {
     Get("/catalogue/search/books?q=banana") ~> routes ~> check {
       status should equal(StatusCodes.OK)
-      responseAs[BookSearchResponse].suggestions shouldBe empty
+      responseAs[BookSearchResponse].suggestions should equal(None)
     }
   }
 }
