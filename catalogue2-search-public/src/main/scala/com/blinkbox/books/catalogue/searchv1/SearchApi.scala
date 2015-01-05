8bb04dfadbaebeb74571d3c3f6015c6ae7ccf619
Aldo Stracquadanio <aldos@blinkbox.com>
2015-01-05 10:48:21 +0000

    Improve routes readability

--- catalogue2-search-public/src/main/scala/com/blinkbox/books/catalogue/searchv1/SearchApi.scala
+++ catalogue2-search-public/src/main/scala/com/blinkbox/books/catalogue/searchv1/SearchApi.scala
@@ -2,11 +2,12 @@ package com.blinkbox.books.catalogue.searchv1
 
 import akka.actor.ActorRefFactory
 import com.blinkbox.books.catalogue.searchv1.V1SearchService.PaginableResponse
-import com.blinkbox.books.config.{ApiConfig, RichConfig}
+import com.blinkbox.books.config.{ ApiConfig, RichConfig }
 import com.blinkbox.books.logging.DiagnosticExecutionContext
-import com.blinkbox.books.spray.{Directives, _}
+import com.blinkbox.books.spray.{ Directives, _ }
 import org.slf4j.LoggerFactory
-import spray.http.{MediaTypes, StatusCodes, Uri}
+import shapeless.HNil
+import spray.http.{ MediaTypes, StatusCodes, Uri }
 import spray.httpx.marshalling.BasicMarshallers
 import spray.routing._
 import scala.concurrent.duration.FiniteDuration
@@ -37,9 +38,9 @@ object SearchApiConfig {
 }
 
 class SearchApi(apiConfig: ApiConfig, searchConfig: SearchApiConfig, searchService: V1SearchService)(implicit val actorRefFactory: ActorRefFactory)
-    extends HttpService
-    with Directives
-    with Serialization {
+  extends HttpService
+  with Directives
+  with Serialization {
 
   implicit val log = LoggerFactory.getLogger(classOf[SearchApi])
   implicit val executionContext = DiagnosticExecutionContext(actorRefFactory.dispatcher)
@@ -53,54 +54,54 @@ class SearchApi(apiConfig: ApiConfig, searchConfig: SearchApiConfig, searchServi
   val validSpecialChars = "-,.';!"
   def preProcess(q: String): String = q.filter(c => c.isLetterOrDigit || validSpecialChars.contains(c) || c.isWhitespace).trim
 
-  val serviceRoutes: Route = get {
-    pathPrefix("catalogue" / "search") {
-      pathPrefix("books") {
-        pathEnd {
-          get {
-            orderedAndPaged(defaultOrder = SortOrder(field="relevance", desc=true), defaultCount = searchConfig.searchDefaultCount) { (order, page) =>
-              validateSortOrder(order.field) {
-                parameter('q ? "") { q =>
-                  validate(!q.trim.isEmpty, "Missing search query term") {
-                    val query = preProcess(q)
-                    validate(!query.isEmpty, "Invalid or empty search term") {
-                      onSuccess(searchService.search(query, page, order)) { res =>
-                        cacheable(searchConfig.maxAge) {
-                          completePaged(page)(res.copy(id = q))
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        } ~
-        path(Segment / "similar") { rawBookId =>
-          validate(rawBookId.forall(_.isDigit) && rawBookId.length == 13, s"Invalid ID: $rawBookId") {
-            get {
-              paged(searchConfig.similarDefaultCount) { page =>
-                onSuccess(searchService.similar(BookId(rawBookId), page)) { res =>
-                  cacheable(searchConfig.maxAge) {
-                    completePaged(page)(res)
-                  }
-                }
-              }
-            }
-          }
-        }
-      } ~
-      path("suggestions") {
-        get {
-          parameter('q, 'limit.as[Int].?) { (q, limit) =>
-            validate(limit.fold(true)(_ > 0), "The limit parameter must be greater than 0 if provided") {
-              cacheable(searchConfig.maxAge) {
-                complete(searchService.suggestions(q, limit getOrElse searchConfig.suggestionsDefaultCount))
-              }
+  val validatedQuery = parameter('q ? "").flatMap { raw =>
+    validate(!raw.trim.isEmpty, "Missing search query term").hflatMap { _ =>
+      val q = preProcess(raw)
+      validate(!q.isEmpty, "Invalid or empty search term").hflatMap { _ =>
+        hprovide(raw :: q :: HNil)
+      }
+    }
+  }
+
+  val bookSearchRoute =
+    orderedAndPaged(defaultOrder = SortOrder(field = "relevance", desc = true), defaultCount = searchConfig.searchDefaultCount) { (order, page) =>
+      validateSortOrder(order.field) {
+        validatedQuery { (raw, query) =>
+          onSuccess(searchService.search(query, page, order)) { res =>
+            cacheable(searchConfig.maxAge) {
+              completePaged(page)(res.copy(id = raw))
             }
           }
         }
       }
+    }
+
+  def similarBooksRoute(rawBookId: String) =
+    validate(rawBookId.forall(_.isDigit) && rawBookId.length == 13, s"Invalid ID: $rawBookId") {
+      paged(searchConfig.similarDefaultCount) { page =>
+        onSuccess(searchService.similar(BookId(rawBookId), page)) { res =>
+          cacheable(searchConfig.maxAge) {
+            completePaged(page)(res)
+          }
+        }
+      }
+    }
+
+  val suggestionsRoute =
+    parameter('q, 'limit.as[Int].?) { (q, limit) =>
+      validate(limit.fold(true)(_ > 0), "The limit parameter must be greater than 0 if provided") {
+        cacheable(searchConfig.maxAge) {
+          complete(searchService.suggestions(q, limit getOrElse searchConfig.suggestionsDefaultCount))
+        }
+      }
+    }
+
+  val serviceRoutes: Route = get {
+    pathPrefix("catalogue" / "search") {
+      pathPrefix("books") {
+        pathEnd(bookSearchRoute) ~ path(Segment / "similar")(similarBooksRoute)
+      } ~
+      path("suggestions")(suggestionsRoute)
     }
   }
 
@@ -128,7 +129,7 @@ class SearchApi(apiConfig: ApiConfig, searchConfig: SearchApiConfig, searchServi
       }
     }
   }
-  
+
   private val PermittedOrderVals = Seq("relevance", "author", "popularity", "price", "publication_date")
 
   private def validateSortOrder(order: String) = validate(
